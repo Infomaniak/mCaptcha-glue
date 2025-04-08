8b69531d75b2f74be49bd540a87e90f4e63e9ce3
Aurélien Bloch <aurelien.bloch@infomaniak.com>
2025-04-08 17:27:22 +0200

    chore(vanilla): [TRELLO-Bgq5pSnt] Handle multiple widgets on the same page

--- packages/vanilla/src/widget.ts
+++ packages/vanilla/src/widget.ts
@@ -4,10 +4,14 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-import { SiteKey, WidgetConfig, ConfigurationError } from "@mcaptcha/core-glue";
-import Receiver from "@mcaptcha/core-glue";
+import Receiver, {SiteKey} from "@mcaptcha/core-glue";
 
 import { ID, INPUT_NAME, INPUT_LABEL_ID } from "./const";
+class WidgetConfig {
+  suffix?: string;
+  siteKey?: SiteKey;
+  widgetLink?: URL;
+}
 
 class Widget {
   inputElement: HTMLInputElement;
@@ -17,27 +21,35 @@ class Widget {
     this.receiver = new Receiver(config, this.setToken);
     this.receiver.listen();
 
-    const parentElement = document.getElementById(ID);
+    const suffix = config.suffix || '';
+    const parentId = `${ID}${suffix}`;
+    const inputId = `${INPUT_NAME}${suffix}`;
+    const labelId = `${INPUT_LABEL_ID}${suffix}`;
+
+    const parentElement = document.getElementById(parentId);
+
     if (parentElement === null || parentElement === undefined) {
-      throw new Error(`Element ${ID}'s parent element is undefined`);
+      throw new Error(`Element ${inputId}'s parent element, ${parentId} is undefined`);
     }
 
     let label = <HTMLLabelElement | null>(
-      document.getElementById(INPUT_LABEL_ID)
+        document.getElementById(labelId)
     );
+
     if (label !== null) {
       label.style.display = "none";
     }
-    this.inputElement = <HTMLInputElement>document.getElementById(INPUT_NAME);
 
-    this.inputElement.id = INPUT_NAME;
-    this.inputElement.name = INPUT_NAME;
+    this.inputElement = <HTMLInputElement>document.getElementById(inputId);
+
+    this.inputElement.id = inputId;
+    this.inputElement.name = inputId;
     this.inputElement.hidden = true;
     this.inputElement.required = true;
     this.inputElement.style.display = "none";
     parentElement.appendChild(this.inputElement);
 
-    const iframe_id = "mcaptcha-widget__iframe";
+    const iframe_id = `mcaptcha-widget__iframe${suffix}`;
     const iframe = document.createElement("iframe");
     iframe.title = "mCaptcha";
     iframe.src = this.receiver.widgetLink.toString();
@@ -54,8 +66,8 @@ class Widget {
         (<any>iframe).sandbox.add("allow-popups");
       } catch {
         iframe.setAttribute(
-          "sandbox",
-          "allow-same-origin allow-scripts allow-popups"
+            "sandbox",
+            "allow-same-origin allow-scripts allow-popups"
         );
       }
     }
@@ -73,20 +85,28 @@ class Widget {
 }
 
 export const run = () => {
-  let label = <HTMLElement | null>document.getElementById(INPUT_LABEL_ID);
+  const inputLabels = <HTMLElement[]>Array.from(document.querySelectorAll(`[id^=${INPUT_LABEL_ID}]:not([data-setup])`));
 
-  if (label !== null && label.dataset.mcaptcha_url) {
-    let config = {
-      widgetLink: new URL(label.dataset.mcaptcha_url),
-    };
-    new Widget(config);
-  } else {
-    throw new Error(
-      `Couldn't find "mcaptcha_url" dataset in element (ID=${INPUT_LABEL_ID})`
-    );
+  for (const inputLabel of inputLabels) {
+    const mcaptchaUrl = inputLabel.dataset.mcaptcha_url
+
+    // Prevents duplicate widget setup if the <script>
+    // tag is included multiple times on the page
+    inputLabel.setAttribute('data-setup', 'true');
+
+    if (mcaptchaUrl) {
+      const config = {
+        suffix: inputLabel.id.replace(INPUT_LABEL_ID, ''),
+        widgetLink: new URL(mcaptchaUrl),
+      };
+      new Widget(config);
+    } else {
+      throw new Error(
+          `Couldn't find "mcaptcha_url" dataset in element (ID=${inputLabel?.id || INPUT_LABEL_ID})`
+      );
+    }
   }
 };
-
 
 
 export default Widget;

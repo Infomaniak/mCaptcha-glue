91274460bb5085ebdc0d917f382a6d449b828c69
Aurélien Bloch <aurelien.bloch@infomaniak.com>
2025-04-10 15:19:07 +0200

    chore(vanilla): Use classes instead of ids

--- packages/vanilla/src/widget.ts
+++ packages/vanilla/src/widget.ts
@@ -4,58 +4,52 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-import Receiver, {SiteKey} from "@mcaptcha/core-glue";
+import Receiver from "@mcaptcha/core-glue";
 
-import { ID, INPUT_NAME, INPUT_LABEL_ID } from "./const";
-class WidgetConfig {
-  suffix?: string;
-  siteKey?: SiteKey;
-  widgetLink?: URL;
-}
+import {ID, INPUT_NAME} from "./const";
 
 class Widget {
   inputElement: HTMLInputElement;
   receiver: Receiver;
 
-  constructor(config: WidgetConfig) {
-    this.receiver = new Receiver(config, this.setToken);
+  constructor(inputElement: HTMLInputElement) {
+    const labelElement = inputElement?.parentElement;
+    console.log(labelElement);
+
+    if (!labelElement || !labelElement.dataset.mcaptcha_url) {
+      throw new Error("Could not find the mcaptcha_url data");
+    }
+
+    this.receiver = new Receiver({
+      widgetLink: new URL(labelElement.dataset.mcaptcha_url),
+    }, this.setToken);
+
     this.receiver.listen();
 
-    const suffix = config.suffix || '';
-    const parentId = `${ID}${suffix}`;
-    const inputId = `${INPUT_NAME}${suffix}`;
-    const labelId = `${INPUT_LABEL_ID}${suffix}`;
-
-    const parentElement = document.getElementById(parentId);
-
-    if (parentElement === null || parentElement === undefined) {
-      throw new Error(`Element ${inputId}'s parent element, ${parentId} is undefined`);
+    if (!labelElement.parentElement) {
+      throw new Error("Captcha's parent element not found");
     }
 
-    let label = <HTMLLabelElement | null>(
-        document.getElementById(labelId)
-    );
+    const containerElement = labelElement.parentElement.querySelector(`:scope > .${ID},:scope > #${ID}`)
 
-    if (label !== null) {
-      label.style.display = "none";
+    if (!containerElement) {
+      throw new Error("Captcha's container element not found");
     }
 
-    this.inputElement = <HTMLInputElement>document.getElementById(inputId);
+    labelElement.style.display = "none";
 
-    this.inputElement.id = inputId;
-    this.inputElement.name = inputId;
+    this.inputElement = inputElement
+
     this.inputElement.hidden = true;
     this.inputElement.required = true;
     this.inputElement.style.display = "none";
-    parentElement.appendChild(this.inputElement);
 
-    const iframe_id = `mcaptcha-widget__iframe${suffix}`;
+    containerElement.appendChild(this.inputElement);
+
     const iframe = document.createElement("iframe");
     iframe.title = "mCaptcha";
     iframe.src = this.receiver.widgetLink.toString();
     iframe.ariaRoleDescription = "presentation";
-    iframe.name = iframe_id;
-    iframe.id = iframe_id;
     iframe.scrolling = "no";
     try {
       (<any>iframe).sandbox = "allow-same-origin allow-scripts allow-popups";
@@ -75,7 +69,7 @@ class Widget {
     iframe.height = "100%";
     iframe.frameBorder = "0";
 
-    parentElement.appendChild(iframe);
+    containerElement.appendChild(iframe);
   }
 
   /*
@@ -85,26 +79,17 @@ class Widget {
 }
 
 export const run = () => {
-  const inputLabels = <HTMLElement[]>Array.from(document.querySelectorAll(`[id^=${INPUT_LABEL_ID}]:not([data-setup])`));
+  const inputs = <HTMLInputElement[]>Array.from(document.querySelectorAll(`input.${INPUT_NAME}:not([data-setup]), input#${INPUT_NAME}:not([data-setup])`));
 
-  for (const inputLabel of inputLabels) {
-    const mcaptchaUrl = inputLabel.dataset.mcaptcha_url
+  if (inputs.length === 0) {
+    throw new Error("Could not find any mCaptcha to setup");
+  }
 
+  for (const input of inputs) {
     // Prevents duplicate widget setup if the <script>
     // tag is included multiple times on the page
-    inputLabel.setAttribute('data-setup', 'true');
-
-    if (mcaptchaUrl) {
-      const config = {
-        suffix: inputLabel.id.replace(INPUT_LABEL_ID, ''),
-        widgetLink: new URL(mcaptchaUrl),
-      };
-      new Widget(config);
-    } else {
-      throw new Error(
-          `Couldn't find "mcaptcha_url" dataset in element (ID=${inputLabel?.id || INPUT_LABEL_ID})`
-      );
-    }
+    input.setAttribute('data-setup', 'true');
+    new Widget(input);
   }
 };
 
